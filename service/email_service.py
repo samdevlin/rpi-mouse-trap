9b953298e4d0f676c807d68149029e679bbaaff6
Sam Devlin <devsoftsam@gmail.com>
2023-11-24 15:47:50 +0100

    Adding TODO for email changes

--- service/email_service.py
+++ service/email_service.py
@@ -6,10 +6,11 @@ from util.error.NoRecipientsError import NoRecipientsError
 EMAIL_REGEX = re.compile(r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?")
 recipient_list = []
 
-#=== GMAIL ACCOUNT DETAILS ===#
+# === GMAIL ACCOUNT DETAILS ===#
 # TODO: move this to a config file
 SENDER_USERNAME = 'ratsout.notification'
 SENDER_APP_KEY = ''
+
 
 def add_recipient(email_addr):
     if _isValidEmail(email_addr) is None:
@@ -18,17 +19,19 @@ def add_recipient(email_addr):
     recipient_list.append(email_addr)
     return recipient_list
 
+
 def notify():
     if not recipient_list:
         raise NoRecipientsError("No email recipients found. Could not notify via email.")
 
     _sendEmails()
 
+
 def _isValidEmail(email):
     return re.fullmatch(EMAIL_REGEX, email)
 
-def _sendEmails():
 
+def _sendEmails():
     # Message Configuration
     msg = MIMEText("Here there's a mouse in the attic ye")
     msg["Subject"] = "You've got mouse"
@@ -37,9 +40,10 @@ def _sendEmails():
 
     # SMTP Config/Execution
     smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtp_server.login(SENDER_USERNAME, SENDER_APP_KEY)
     try:
+        smtp_server.login(SENDER_USERNAME, SENDER_APP_KEY)
         smtp_server.send_message(msg)
+    # TODO: catch timeout
     except Exception as err:
         print(f"An unexpected error of type {type(err)} occurred: {err}")
     finally:
