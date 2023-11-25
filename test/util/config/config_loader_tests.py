8b061c139f33a7239ad6b2d7318428f61122e18d
samdevlin <devsoftsam@gmail.com>
2023-11-26 00:48:17 +0100

    Fixed env loader for config file

---
+++ test/util/config/config_loader_tests.py
@@ -0,0 +1,13 @@
+import unittest, os
+from util.config.config_loader import load_section_to_env
+
+
+class TestSuccessCases(unittest.TestCase):
+    def test_config_is_loaded(self):
+        load_section_to_env('TEST')
+        self.assertEqual(os.environ['username'], 'test_user')
+        self.assertEqual(os.environ['app_key'], 'test_key')
+
+
+if __name__ == '__main__':
+    unittest.main()
--- util/config/config_loader.py
+++ util/config/config_loader.py
@@ -4,12 +4,13 @@ from util.error.MissingConfigurationError import MissingConfigurationError
 
 def load_section_to_env(section_name):
     config = configparser.ConfigParser()
-    config.read('../../config.ini')
+    print(os.getcwd())
+    config.read('./config.ini')
     if not config.sections():
         raise MissingConfigurationError("No config.ini file found. Could not load configuration.")
     elif not section_name in config:
         raise MissingConfigurationError(
             "No EMAIL section found in configuration. Make sure username & app_key are set.")
 
-    for k, v in config[section_name]:
-        os.environ[k] = v
+    for item in config[section_name].items():
+        os.environ[item[0]] = item[1]
